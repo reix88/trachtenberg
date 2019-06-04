d134e3303ee4c229d0dfb2d453e7e89a8ec3d335
akashi-root <wildhunter404@gmail.com>
2019-06-05 01:33:33 +0500

    Initial Commit

--- berg.py
+++ berg.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 
 # |------------------------------------|
-# |TRAXTENBERG-SYSTEM UNREAL PRIMERS...|
+# | TRACHTENBERG-SYSTEM UNREAL PRIMERS |
 # |------------------------------------|
 
 import random
 
-# Author: Anti-Social
+# Author: Akashi
 # Creation Date: 31.12.2018
 # Last Change: 05.01.2019
 # Verion: 1.3
-# Созданно при поддержке Lil Uzi Vert & Scarlxrd.
+# Созданно при поддержке Lil Uzi Vert.
 
 pravilno = 0
 ne_pravilno = 0
@@ -20,20 +20,17 @@ def schet(one , two):
     global ne_pravilno
 
     if one == two:
-        pravilno = pravilno + 1
+        pravilno += 1
     else:
-        ne_pravilno = ne_pravilno + 1
+        ne_pravilno += 1
 
 input("Press Enter to START...")
 
 print("\n----------------\nУМНОЖЕНИЕ НА 11:\n----------------\n")
 
-i = 0
-while i < 5:
-    i = i + 1
-
-    int_1 = random.randrange(101, 10000, 1)
-    print("Пример № " + str(i) + ":  " + str(int_1) + " x 11 = ?")
+for i in range(5):
+    int_1 = random.randrange(101, 10000)
+    print("Пример № " + str(i+1) + ":  " + str(int_1) + " x 11 = ?")
             
     while True:
         try:
@@ -46,15 +43,11 @@ while i < 5:
     src = int_1 * 11
     schet(otv, src)
 
-
 print("\n----------------\nУМНОЖЕНИЕ НА 12:\n----------------\n")
 
-i = 0
-while i < 5:
-    i = i + 1
-
-    int_1 = random.randrange(101, 10000, 1)
-    print("Пример № " + str(i) + ":  " + str(int_1) + " x 12 = ?")
+for i in range(5):
+    int_1 = random.randrange(101, 10000)
+    print("Пример № " + str(i+1) + ":  " + str(int_1) + " x 12 = ?")
     
     while True:
         try:
@@ -67,15 +60,11 @@ while i < 5:
     src = int_1 * 12
     schet(otv, src)
 
-
 print("\n---------------\nУМНОЖЕНИЕ НА 9:\n---------------\n")
 
-i = 0
-while i < 5:
-    i = i + 1
-
-    int_1 = random.randrange(101, 10000, 1)
-    print("Пример № " + str(i) + ":  " + str(int_1) + " x 9 = ?")
+for i in range(5):
+    int_1 = random.randrange(101, 10000)
+    print("Пример № " + str(i+1) + ":  " + str(int_1) + " x 9 = ?")
     
     while True:
         try:
@@ -88,15 +77,11 @@ while i < 5:
     src = int_1 * 9
     schet(otv, src)
 
-
 print("\n---------------\nУМНОЖЕНИЕ НА 8:\n---------------\n")
 
-i = 0
-while i < 5:
-    i = i + 1
-
-    int_1 = random.randrange(101, 10000, 1)
-    print("Пример № " + str(i) + ":  " + str(int_1) + " x 8 = ?")
+for i in range(5):
+    int_1 = random.randrange(101, 10000)
+    print("Пример № " + str(i+1) + ":  " + str(int_1) + " x 8 = ?")
     
     while True:
         try:
@@ -109,16 +94,12 @@ while i < 5:
     src = int_1 * 8
     schet(otv, src)
 
-
 print("\n-----------------------------------\nУМНОЖЕНИЕ ДВУЗНАЧНЫХ НА ДВУЗНАЧНЫЕ:\n-----------------------------------\n")
 
-i = 0
-while i < 5:
-    i = i + 1
-
-    int_1 = random.randrange(10, 100, 1)
-    int_2 = random.randrange(10, 100, 1)
-    print("Пример № " + str(i) + ":  " + str(int_1) + " x " + str(int_2) + " = ?")
+for i in range(5):
+    int_1 = random.randrange(10, 100)
+    int_2 = random.randrange(10, 100)
+    print("Пример № " + str(i+1) + ":  " + str(int_1) + " x " + str(int_2) + " = ?")
     
     while True:
         try:
@@ -130,17 +111,13 @@ while i < 5:
             
     src = int_1 * int_2
     schet(otv, src)
-
 
 print("\n----------------------\nМНОГОЗНАЧНЫЕ МНОЖИМЫЕ:\n----------------------\n")
 
-i = 0
-while i < 5:
-    i = i + 1
-
-    int_1 = random.randrange(100, 1000, 1)
-    int_2 = random.randrange(10, 100, 1)
-    print("Пример № " + str(i) + ":  " + str(int_1) + " x " + str(int_2) + " = ?")
+for i in range(5):
+    int_1 = random.randrange(100, 1000)
+    int_2 = random.randrange(10, 100)
+    print("Пример № " + str(i+1) + ":  " + str(int_1) + " x " + str(int_2) + " = ?")
     
     while True:
         try:
@@ -152,17 +129,13 @@ while i < 5:
             
     src = int_1 * int_2
     schet(otv, src)
-
 
 print("\n----------------------\nУМНОЖЕНИЕ ЛЮБОЙ ДЛИНЫ:\n----------------------\n")
 
-i = 0
-while i < 5:
-    i = i + 1
-
-    int_1 = random.randrange(100, 99000, 1)
-    int_2 = random.randrange(100, 2000, 1)
-    print("Пример № " + str(i) + ":  " + str(int_1) + " x " + str(int_2) + " = ?")
+for i in range(5):
+    int_1 = random.randrange(100, 99000)
+    int_2 = random.randrange(100, 999)
+    print("Пример № " + str(i+1) + ":  " + str(int_1) + " x " + str(int_2) + " = ?")
     
     while True:
         try:
@@ -175,59 +148,51 @@ while i < 5:
     src = int_1 * int_2
     schet(otv, src)
 
-
 print("\n------------------------------\nВОЗВЕДЕНИЕ В КВ. (ОКАН. НА 5):\n------------------------------\n")
 
-i = 0
-while i < 9:
-    arr_five = [15, 25, 35, 45, 55, 65, 75, 85, 95]
+arr_five = [15, 25, 35, 45, 55, 65, 75, 85, 95]
 
-    for x in arr_five:
-        print("Пример № " + str(i) + ":  " + str(x) + " x " + str(x) + " = ?")
-        
-        while True:
-            try:
-                otv = int(input())
-            except ValueError:
-                print('Введите число!')
-            else:
-                break
-            
-        src = x * x
-        i = i + 1
-        schet(otv, src)
-
+for i in range(9):
+	x = random.choice(arr_five)
+	print("Пример № " + str(i+1) + ":  " + str(x) + " x " + str(x) + " = ?")
+	
+	while True:
+		try:
+			otv = int(input())
+		except ValueError:
+			print('Введите число!')
+		else:
+			break
+	
+	src = x * x
+	schet(otv, src)
+	arr_five.remove(x)
 
 print("\n-----------------------------\nВОЗВЕДЕНИЕ В КВ. (НАЧ. НА 5):\n-----------------------------\n")
 
-i = 0
-while i < 8:
-    arr_five2 = [51, 52, 53, 54, 56, 57, 58, 59]
+arr_five2 = [51, 52, 53, 54, 56, 57, 58, 59]
 
-    for x in arr_five2:
-        print("Пример № " + str(i) + ":  " + str(x) + " x " + str(x) + " = ?")
-        
-        while True:
-            try:
-                otv = int(input())
-            except ValueError:
-                print('Введите число!')
-            else:
-                break
-            
-        src = x * x
-        i = i + 1
-        schet(otv, src)
-
+for i in range(8):
+	x = random.choice(arr_five2)
+	print("Пример № " + str(i+1) + ":  " + str(x) + " x " + str(x) + " = ?")
+	
+	while True:
+		try:
+			otv = int(input())
+		except ValueError:
+			print('Введите число!')
+		else:
+			break
+		
+	src = x * x
+	schet(otv, src)
+	arr_five2.remove(x)
 
 print("\n---------------------\nВОЗВЕДЕНИЕ В КВАДРАТ:\n---------------------\n")
 
-i = 0
-while i < 6:
-    i = i + 1
-
-    int_1 = random.randrange(10, 100, 1)
-    print("Пример № " + str(i) + ":  " + str(int_1) + " x " + str(int_1) + " = ?")
+for i in range(6):
+    int_1 = random.randrange(10, 100)
+    print("Пример № " + str(i+1) + ":  " + str(int_1) + " x " + str(int_1) + " = ?")
     
     while True:
         try:
